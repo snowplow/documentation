ea539f2cf316810e729e38a60f1d9e8ec9e711de
Andy Hay <3116331+Andy-Hay@users.noreply.github.com>
2022-07-28 14:05:04 +0100

    Initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Snowplow Docs',
   tagline: '[Insert Tag line here]',
   url: 'https://andy-hay.github.io',
-  baseUrl: '/',
+  baseUrl: '/docsite-poc.github.io/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',

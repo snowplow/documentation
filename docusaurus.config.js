472a077643ee70b02989cb702763264c887b2d3c
Andy Hay <3116331+Andy-Hay@users.noreply.github.com>
2022-07-28 12:06:15 +0100

    Initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Andy-Hay', // Usually your GitHub org/user name.
+  projectName: 'docsite-repo.github.io', // Usually your repo name.
+  deploymentBranch: 'main',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -69,7 +70,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',

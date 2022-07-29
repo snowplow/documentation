09b0219084e164f7a709957b0f52d7698f749885
Andy Hay <3116331+Andy-Hay@users.noreply.github.com>
2022-07-29 14:17:42 +0100

    Add badges and highlight templates

---
+++ src/components/Badges.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default function Badges(props) {
+  if (props.badgeType === 'Actively Maintained') {
+    return (
+      <img
+        loading="lazy"
+        src="https://img.shields.io/static/v1?style=flat&amp;label=Snowplow&amp;message=Actively%20Maintained&amp;color=6638b8&amp;labelColor=9ba0aa&amp;logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAeFBMVEVMaXGXANeYANeXANZbAJmXANeUANSQAM+XANeMAMpaAJhZAJeZANiXANaXANaOAM2WANVnAKWXANZ9ALtmAKVaAJmXANZaAJlXAJZdAJxaAJlZAJdbAJlbAJmQAM+UANKZANhhAJ+EAL+BAL9oAKZnAKVjAKF1ALNBd8J1AAAAKHRSTlMAa1hWXyteBTQJIEwRgUh2JjJon21wcBgNfmc+JlOBQjwezWF2l5dXzkW3/wAAAHpJREFUeNokhQOCA1EAxTL85hi7dXv/E5YPCYBq5DeN4pcqV1XbtW/xTVMIMAZE0cBHEaZhBmIQwCFofeprPUHqjmD/+7peztd62dWQRkvrQayXkn01f/gWp2CrxfjY7rcZ5V7DEMDQgmEozFpZqLUYDsNwOqbnMLwPAJEwCopZxKttAAAAAElFTkSuQmCC"
+        alt="Actively Maintained"
+        title="Actively Maintained badge"
+        class="img_ev3q"
+      ></img>
+    );
+  }
+  if (props.badgeType === 'Early Release') {
+    return (
+      <img
+        loading="lazy"
+        src="https://img.shields.io/static/v1?style=flat&amp;label=Snowplow&amp;message=Early%20Release&amp;color=014477&amp;labelColor=9ba0aa&amp;logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAeFBMVEVMaXGXANeYANeXANZbAJmXANeUANSQAM+XANeMAMpaAJhZAJeZANiXANaXANaOAM2WANVnAKWXANZ9ALtmAKVaAJmXANZaAJlXAJZdAJxaAJlZAJdbAJlbAJmQAM+UANKZANhhAJ+EAL+BAL9oAKZnAKVjAKF1ALNBd8J1AAAAKHRSTlMAa1hWXyteBTQJIEwRgUh2JjJon21wcBgNfmc+JlOBQjwezWF2l5dXzkW3/wAAAHpJREFUeNokhQOCA1EAxTL85hi7dXv/E5YPCYBq5DeN4pcqV1XbtW/xTVMIMAZE0cBHEaZhBmIQwCFofeprPUHqjmD/+7peztd62dWQRkvrQayXkn01f/gWp2CrxfjY7rcZ5V7DEMDQgmEozFpZqLUYDsNwOqbnMLwPAJEwCopZxKttAAAAAElFTkSuQmCC"
+        alt="Early Release"
+        title="Early Release badge"
+        class="img_ev3q"
+      ></img>
+    );
+  }
+  if (props.badgeType === 'Snowplow Tracker Release') {
+    return (
+      <img
+        src="https://img.shields.io/packagist/v/snowplow/snowplow-tracker"
+        alt="Latest version"
+      ></img>
+    );
+  }
+  if (props.badgeType === 'Snowplow Tracker PHP Release') {
+    return (
+      <img
+        src="https://img.shields.io/packagist/php-v/snowplow/snowplow-tracker"
+        alt="PHP Version"
+      ></img>
+    );
+  }
+}

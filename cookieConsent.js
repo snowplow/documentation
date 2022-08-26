b54e8ea5aad8a99c6d74dbc1d2e86b6e71a89cd8
Joe <joe.miskolczy@snowplowanalytics.com>
2022-08-26 13:49:44 +0200

    Fix build issues

--- cookieConsent.js
+++ cookieConsent.js
@@ -1,3 +1,4 @@
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
 import { configure, init } from 'cookie-though'
 import Cookies from 'js-cookie'
 import { COOKIE_PREF_KEY } from './src/constants/config'
@@ -35,17 +36,19 @@ const cookieConfig = {
   "customizeLabel": "Edit preferences"
 }
 
-// It's necessary to call this function first before using any of the exported methods from the cookie-though package
-// as each method checks if there's a config object set already and if there isn't they show the popup
-// even when preferences were already set previously. So we can prevent that with this call.
-// The load order of client modules matter because of this as the snowplow / google tracker modules
-// also import and use methods from cookie-though that need the config already set.
-// See: https://github.com/inthepocket/cookie-though/blob/a90d059e0d2e81d612c9bb4afb40f6612bdf02c3/src/lib.ts#L55
-configure(cookieConfig)
+if(ExecutionEnvironment.canUseDOM) {
+  // It's necessary to call this function first before using any of the exported methods from the cookie-though package
+  // as each method checks if there's a config object set already and if there isn't they show the popup
+  // even when preferences were already set previously. So we can prevent that with this call.
+  // The load order of client modules matter because of this as the snowplow / google tracker modules
+  // also import and use methods from cookie-though that need the config already set.
+  // See: https://github.com/inthepocket/cookie-though/blob/a90d059e0d2e81d612c9bb4afb40f6612bdf02c3/src/lib.ts#L55
+  configure(cookieConfig)
 
-const cookiePreferences = Cookies.get(COOKIE_PREF_KEY)
+  const cookiePreferences = Cookies.get(COOKIE_PREF_KEY)
 
-if (!cookiePreferences) {
-  // this is what displays the cookie preferences popup
-  init(cookieConfig)
+  if (!cookiePreferences) {
+    // this is what displays the cookie preferences popup
+    init(cookieConfig)
+  }
 }

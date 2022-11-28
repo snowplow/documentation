0d86d831e9be33643214178628cc61292256749c
Nick <nick.stanch@snowplowanalytics.com>
2022-11-28 11:46:27 +0000

    Revert "Added proper link refreshing for the tracker (#200)"
    This reverts commit 675c5fc3aa223512e9c69395014c8b7d1aba85f9.
    The reason is that it broke the sidebar collapsing function.

---
+++ src/theme/DocSidebar/Desktop/Content/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Content from '@theme-original/DocSidebar/Desktop/Content'
+import styles from './styles.module.css'
+
+export default function ContentWrapper(props) {
+  return (
+    <>
+      <Content {...props} className={styles.extraPadding} />
+    </>
+  )
+}

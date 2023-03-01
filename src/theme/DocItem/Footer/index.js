a424b566692ad395c42d2ecf79429ca010a11ac7
Miranda Wilson <miranda@snowplowanalytics.com>
2023-03-01 17:43:53 +0000

    Add basic feedback widget

---
+++ src/theme/DocItem/Footer/index.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import clsx from 'clsx'
+import { ThemeClassNames } from '@docusaurus/theme-common'
+import { useDoc } from '@docusaurus/theme-common/internal'
+import LastUpdated from '@theme/LastUpdated'
+import EditThisPage from '@theme/EditThisPage'
+import TagsListInline from '@theme/TagsListInline'
+import styles from './styles.module.css'
+import { trackStructEvent } from '@snowplow/browser-tracker'
+
+function TagsRow(props) {
+  return (
+    <div
+      className={clsx(
+        ThemeClassNames.docs.docFooterTagsRow,
+        'row margin-bottom--sm'
+      )}
+    >
+      <div className="col">
+        <TagsListInline {...props} />
+      </div>
+    </div>
+  )
+}
+
+function Feedback() {
+  return (
+    <div className="col margin-bottom--sm">
+      Was this page helpful?
+      <div>
+        <button
+          onClick={trackStructEvent({
+            category: 'feedback',
+            action: 'like',
+          })}
+        >
+          Yes
+        </button>
+        <button
+          onClick={trackStructEvent({
+            category: 'feedback',
+            action: 'dislike',
+          })}
+        >
+          No
+        </button>
+      </div>
+    </div>
+  )
+}
+
+function EditMetaRow({
+  editUrl,
+  lastUpdatedAt,
+  lastUpdatedBy,
+  formattedLastUpdatedAt,
+}) {
+  return (
+    <div className={clsx(ThemeClassNames.docs.docFooterEditMetaRow, 'row')}>
+      <div className="col">{editUrl && <EditThisPage editUrl={editUrl} />}</div>
+
+      <div className={clsx('col', styles.lastUpdated)}>
+        {(lastUpdatedAt || lastUpdatedBy) && (
+          <LastUpdated
+            lastUpdatedAt={lastUpdatedAt}
+            formattedLastUpdatedAt={formattedLastUpdatedAt}
+            lastUpdatedBy={lastUpdatedBy}
+          />
+        )}
+      </div>
+    </div>
+  )
+}
+export default function DocItemFooter() {
+  const { metadata } = useDoc()
+  const {
+    editUrl,
+    lastUpdatedAt,
+    formattedLastUpdatedAt,
+    lastUpdatedBy,
+    tags,
+  } = metadata
+  const canDisplayTagsRow = tags.length > 0
+  const canDisplayEditMetaRow = !!(editUrl || lastUpdatedAt || lastUpdatedBy)
+  const canDisplayFooter = canDisplayTagsRow || canDisplayEditMetaRow
+  if (!canDisplayFooter) {
+    return null
+  }
+  return (
+    <footer
+      className={clsx(ThemeClassNames.docs.docFooter, 'docusaurus-mt-lg')}
+    >
+      <Feedback />
+      {canDisplayTagsRow && <TagsRow tags={tags} />}
+      {canDisplayEditMetaRow && (
+        <EditMetaRow
+          editUrl={editUrl}
+          lastUpdatedAt={lastUpdatedAt}
+          lastUpdatedBy={lastUpdatedBy}
+          formattedLastUpdatedAt={formattedLastUpdatedAt}
+        />
+      )}
+    </footer>
+  )
+}

e9f258027664155bf2b1b5afbf2eba373e6073a9
rlh1994 <rlh1994@users.noreply.github.com>
2023-04-10 08:06:03 +0000

    [create-pull-request] automated change

--- src/dbtVersions.js
+++ src/dbtVersions.js
@@ -458,6 +458,12 @@ export const dbtVersions = {
             "packages": {
                 "dbt-labs/dbt_utils": ">=1.0.0 <2.0.0"
             }
+        },
+        "0.14.1": {
+            "dbtversion": ">=1.4.0 <2.0.0",
+            "packages": {
+                "dbt-labs/dbt_utils": ">=1.0.0 <2.0.0"
+            }
         }
     },
     "snowplow/snowplow_web": {

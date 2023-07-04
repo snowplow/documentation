90a9a5d6e816f4c91f6901079182ccf6b4e30fb0
Ryan Hill <r.l.hill128@gmail.com>
2023-07-04 11:16:23 +0100

    yarn format

--- src/components/JsonSchemaValidator/dbtMobile.js
+++ src/components/JsonSchemaValidator/dbtMobile.js
@@ -1,155 +1,164 @@
 export const dbtSnowplowMobileConfigSchema = {
-    "type": "object",
-    "properties": {
-      "snowplow__atomic_schema": {
-        "type": "string",
-        "title": "Schema",
-        "description": "Schema (dataset) that contains your atomic events"
-      },
-      "snowplow__database": {
-        "type": "string",
-        "title": "Database",
-        "description": "Database that contains your atomic events"
-      },
-      "snowplow__dev_target_name": {
-        "type": "string",
-        "title": "Dev Target",
-        "description": "Target name of your development environment as defined in your `profiles.yml` file"
-      },
-      "snowplow__events_table": {
-        "type": "string",
-        "title": "Events Table",
-        "description": "The name of the table that contains your atomic events"
-      },
-      "snowplow__sessions_table": {
-        "type": "string",
-        "title": "Sessions Table",
-        "description": "The users module requires data from the derived sessions table. If you choose to disable the standard sessions table in favor of your own custom table, set this to reference your new table e.g. {{ ref('snowplow_web_sessions_custom') }}"
-      },
-      "snowplow__allow_refresh": {
-        "type": "boolean",
-        "title": "Allow Refresh"
-      },
-      "snowplow__backfill_limit_days": {
-        "type": "number",
-        "minimum": 0,
-        "title": "Backfill Limit",
-        "description": "The maximum numbers of days of new data to be processed since the latest event processed"
-      },
-      "snowplow__days_late_allowed": {
-        "type": "number",
-        "minimum": 0,
-        "title": "Days Late Allowed",
-        "description": "The maximum allowed number of days between the event creation and it being sent to the collector"
-      },
-      "snowplow__lookback_window_hours": {
-        "type": "number",
-        "minimum": 0,
-        "title": "Event Lookback Window",
-        "description": "The number of hours to look before the latest event processed - to account for late arriving data, which comes out of order"
-      },
-      "snowplow__max_session_days": {
-        "type": "number",
-        "minimum": 0,
-        "title": "Max Session Length",
-        "description": "The maximum allowed session length in days. For a session exceeding this length, all events after this limit will stop being processed"
-      },
-      "snowplow__session_lookback_days": {
-        "type": "number",
-        "minimum": 0,
-        "title": "Session Lookback Window",
-        "description": "Number of days to limit scan on `snowplow_web_base_sessions_lifecycle_manifest` manifest"
-      },
-      "snowplow__session_stitching": {
-        "type": "boolean",
-        "title": "Enable Session Stitching"
-      },
-      "snowplow__start_date": {
-        "type": "string",
-        "format": "date",
-        "title": "Start Date",
-        "description": "The date to start processing events from in the package on first run or a full refresh, based on `collector_tstamp`"
-      },
-      "snowplow__upsert_lookback_days": {
-        "type": "number",
-        "minimum": 0,
-        "title": "Upsert Lookback Days",
-        "description": "Number of days to look back over the incremental derived tables during the upsert"
-      },
-      "snowplow__app_id": {
-        "type": "array",
-        "description": "> Click the plus sign to add a new entry",
-        "minItems": 0,
-        "title": "App IDs",
-        "items": { "type": "string" }
-      },
-      "snowplow__enable_app_errors_module": {
-        "type": "boolean",
-        "title": "Enable App Errors module"
-      },
-      "snowplow__enable_application_context": {
-        "type": "boolean",
-        "title": "Enable Application context"
-      },
-      "snowplow__enable_geolocation_context": {
-        "type": "boolean",
-        "title": "Enable Geolocation context"
-      },
-      "snowplow__enable_mobile_context": {
-        "type": "boolean",
-        "title": "Enable Mobile context"
-      },
-      "snowplow__enable_screen_context": {
-        "type": "boolean",
-        "title": "Enable Screen context"
-      },
-      "snowplow__has_log_enabled": {
-        "type": "boolean",
-        "title": "Enable Run Logs"
-      },
-      "snowplow__platform": {
-        "type": "array",
-        "description": "> Click the plus sign to add a new entry",
-        "minItems": 0,
-        "title": "Platforms",
-        "items": { "type": "string" }
-      },
-      "snowplow__databricks_catalog": {
-        "type": "string",
-        "title": "(Databricks) Catalog",
-        "description": "The catalogue your atomic events table is in"
-      },
-      "snowplow__session_context": {
-        "type": "string",
-        "title": "(Redshift) Session Context Table"
-      },
-      "snowplow__mobile_context": {
-        "type": "string",
-        "title": "(Redshift) Mobile Context Table"
-      },
-      "snowplow__geolocation_context": {
-        "type": "string",
-        "title": "(Redshift) Geolocation Context Table"
-      },
-      "snowplow__application_context": {
-        "type": "string",
-        "title": "(Redshift) Application Context Table"
-      },
-      "snowplow__screen_context": {
-        "type": "string",
-        "title": "(Redshift) Screen Context Table"
-      },
-      "snowplow__app_errors_table": {
-        "type": "string",
-        "title": "(Redshift) App Errors Context Table"
-      },
-      "snowplow__screen_view_events": {
-        "type": "string",
-        "title": "(Redshift) Screen View Events Table"
-      },
-      "snowplow__derived_tstamp_partitioned": {
-        "type": "boolean",
-        "title": "(Bigquery) Dervied Timestamp Partition"
-      }
-    }
-  };
+  type: 'object',
+  properties: {
+    snowplow__atomic_schema: {
+      type: 'string',
+      title: 'Schema',
+      description: 'Schema (dataset) that contains your atomic events',
+    },
+    snowplow__database: {
+      type: 'string',
+      title: 'Database',
+      description: 'Database that contains your atomic events',
+    },
+    snowplow__dev_target_name: {
+      type: 'string',
+      title: 'Dev Target',
+      description:
+        'Target name of your development environment as defined in your `profiles.yml` file',
+    },
+    snowplow__events_table: {
+      type: 'string',
+      title: 'Events Table',
+      description: 'The name of the table that contains your atomic events',
+    },
+    snowplow__sessions_table: {
+      type: 'string',
+      title: 'Sessions Table',
+      description:
+        "The users module requires data from the derived sessions table. If you choose to disable the standard sessions table in favor of your own custom table, set this to reference your new table e.g. {{ ref('snowplow_web_sessions_custom') }}",
+    },
+    snowplow__allow_refresh: {
+      type: 'boolean',
+      title: 'Allow Refresh',
+    },
+    snowplow__backfill_limit_days: {
+      type: 'number',
+      minimum: 0,
+      title: 'Backfill Limit',
+      description:
+        'The maximum numbers of days of new data to be processed since the latest event processed',
+    },
+    snowplow__days_late_allowed: {
+      type: 'number',
+      minimum: 0,
+      title: 'Days Late Allowed',
+      description:
+        'The maximum allowed number of days between the event creation and it being sent to the collector',
+    },
+    snowplow__lookback_window_hours: {
+      type: 'number',
+      minimum: 0,
+      title: 'Event Lookback Window',
+      description:
+        'The number of hours to look before the latest event processed - to account for late arriving data, which comes out of order',
+    },
+    snowplow__max_session_days: {
+      type: 'number',
+      minimum: 0,
+      title: 'Max Session Length',
+      description:
+        'The maximum allowed session length in days. For a session exceeding this length, all events after this limit will stop being processed',
+    },
+    snowplow__session_lookback_days: {
+      type: 'number',
+      minimum: 0,
+      title: 'Session Lookback Window',
+      description:
+        'Number of days to limit scan on `snowplow_web_base_sessions_lifecycle_manifest` manifest',
+    },
+    snowplow__session_stitching: {
+      type: 'boolean',
+      title: 'Enable Session Stitching',
+    },
+    snowplow__start_date: {
+      type: 'string',
+      format: 'date',
+      title: 'Start Date',
+      description:
+        'The date to start processing events from in the package on first run or a full refresh, based on `collector_tstamp`',
+    },
+    snowplow__upsert_lookback_days: {
+      type: 'number',
+      minimum: 0,
+      title: 'Upsert Lookback Days',
+      description:
+        'Number of days to look back over the incremental derived tables during the upsert',
+    },
+    snowplow__app_id: {
+      type: 'array',
+      description: '> Click the plus sign to add a new entry',
+      minItems: 0,
+      title: 'App IDs',
+      items: { type: 'string' },
+    },
+    snowplow__enable_app_errors_module: {
+      type: 'boolean',
+      title: 'Enable App Errors module',
+    },
+    snowplow__enable_application_context: {
+      type: 'boolean',
+      title: 'Enable Application context',
+    },
+    snowplow__enable_geolocation_context: {
+      type: 'boolean',
+      title: 'Enable Geolocation context',
+    },
+    snowplow__enable_mobile_context: {
+      type: 'boolean',
+      title: 'Enable Mobile context',
+    },
+    snowplow__enable_screen_context: {
+      type: 'boolean',
+      title: 'Enable Screen context',
+    },
+    snowplow__has_log_enabled: {
+      type: 'boolean',
+      title: 'Enable Run Logs',
+    },
+    snowplow__platform: {
+      type: 'array',
+      description: '> Click the plus sign to add a new entry',
+      minItems: 0,
+      title: 'Platforms',
+      items: { type: 'string' },
+    },
+    snowplow__databricks_catalog: {
+      type: 'string',
+      title: '(Databricks) Catalog',
+      description: 'The catalogue your atomic events table is in',
+    },
+    snowplow__session_context: {
+      type: 'string',
+      title: '(Redshift) Session Context Table',
+    },
+    snowplow__mobile_context: {
+      type: 'string',
+      title: '(Redshift) Mobile Context Table',
+    },
+    snowplow__geolocation_context: {
+      type: 'string',
+      title: '(Redshift) Geolocation Context Table',
+    },
+    snowplow__application_context: {
+      type: 'string',
+      title: '(Redshift) Application Context Table',
+    },
+    snowplow__screen_context: {
+      type: 'string',
+      title: '(Redshift) Screen Context Table',
+    },
+    snowplow__app_errors_table: {
+      type: 'string',
+      title: '(Redshift) App Errors Context Table',
+    },
+    snowplow__screen_view_events: {
+      type: 'string',
+      title: '(Redshift) Screen View Events Table',
+    },
+    snowplow__derived_tstamp_partitioned: {
+      type: 'boolean',
+      title: '(Bigquery) Dervied Timestamp Partition',
+    },
+  },
+}
--- src/components/JsonSchemaValidator/index.js
+++ src/components/JsonSchemaValidator/index.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { Templates } from '@rjsf/mui';
-import { RJSFSchema } from '@rjsf/utils';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { useColorMode } from '@docusaurus/theme-common';
-import validator from '@rjsf/validator-ajv8';
-import Form from '@rjsf/mui';
-import Details from '@theme/Details';
+import { Templates } from '@rjsf/mui'
+import { RJSFSchema } from '@rjsf/utils'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { useColorMode } from '@docusaurus/theme-common'
+import validator from '@rjsf/validator-ajv8'
+import Form from '@rjsf/mui'
+import Details from '@theme/Details'
 // Allow for grouping of items into collapsible fields
 // Get default object field template to pass to
-export const ObjectFieldTemplates = Templates.ObjectFieldTemplate;
+export const ObjectFieldTemplates = Templates.ObjectFieldTemplate
 
 // generator to make a function that is a object field template valid function
 export const ObjectFieldTemplateGroupsGenerator = (groups) => (props) => {
@@ -18,44 +18,41 @@ export const ObjectFieldTemplateGroupsGenerator = (groups) => (props) => {
       <>
         {groups.map((group) => {
           // filter to just the relevant props
-          const childProps = getPropsForGroup(group, props);
+          const childProps = getPropsForGroup(group, props)
           return (
             <>
               <Details summary={group.title}>
                 <ObjectFieldTemplates key={group.title} {...childProps} />
               </Details>
             </>
-          );
+          )
         })}
       </>
-    );
+    )
   }
-  return (<ObjectFieldTemplates {...props} />);
-};
+  return <ObjectFieldTemplates {...props} />
+}
 
 // Filter props to where they have the name listed in the groups fields
-export const getPropsForGroup = (
-  group,
-  props
-) => {
+export const getPropsForGroup = (group, props) => {
   return {
     ...props,
-    properties: props.properties.filter((p) => group.fields.includes(p.name))
-  };
-};
+    properties: props.properties.filter((p) => group.fields.includes(p.name)),
+  }
+}
 
 // Themeing
 export const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
-});
+})
 
 export const lightTheme = createTheme({
   palette: {
     mode: 'light',
   },
-});
+})
 
 // main applet
 export const JsonApp = (props) => {
@@ -63,8 +60,8 @@ export const JsonApp = (props) => {
   // props.schema - the schema to validate against
   // props.template - the template to use, if provided
   // props.output - a function that renders an output, taking the formData as an input
-  const [formData, setFormData] = React.useState(null);
-  const { colorMode, setColorMode } = useColorMode();
+  const [formData, setFormData] = React.useState(null)
+  const { colorMode, setColorMode } = useColorMode()
   return (
     <>
       <ThemeProvider theme={colorMode === 'dark' ? darkTheme : lightTheme}>
@@ -74,13 +71,20 @@ export const JsonApp = (props) => {
             formData={formData}
             onChange={(e) => setFormData(e.formData)}
             validator={validator}
-            showErrorList='bottom'
-            templates={{ ObjectFieldTemplate: props.template ? props.template : ObjectFieldTemplates }}
+            showErrorList="bottom"
+            templates={{
+              ObjectFieldTemplate: props.template
+                ? props.template
+                : ObjectFieldTemplates,
+            }}
             liveValidate
             {...props}
-          ><div /></Form></div>
+          >
+            <div />
+          </Form>
+        </div>
         {props.output(formData)}
       </ThemeProvider>
     </>
-  );
-};
+  )
+}

644fd6f88f200efb4629a051e4fd3921a2b8654e
Nick <nick.stanch@snowplowanalytics.com>
2023-07-31 16:36:19 +0100

    Add a global entity that tracks which tabs are selected for cloud/warehouse (#557)
    * Add a global entity that tracks which tabs are selected for cloud/warehouse
    * Don’t add the entity when it’s empty

--- snowplow.js
+++ snowplow.js
@@ -2,6 +2,7 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
 import {
   newTracker,
   trackPageView,
+  addGlobalContexts,
   enableActivityTracking,
   disableAnonymousTracking,
 } from '@snowplow/browser-tracker'
@@ -14,6 +15,7 @@ import { onPreferencesChanged } from 'cookie-though'
 import Cookies from 'js-cookie'
 import { COOKIE_PREF_KEY, DOCS_SITE_URLS } from './src/constants/config'
 import { reloadOnce } from './src/helpers/reloadOnce'
+import { isEmpty, pickBy } from 'lodash'
 
 const createTrackerConfig = (cookieName) => {
   const appId = DOCS_SITE_URLS.includes(window.location.hostname)
@@ -53,6 +55,18 @@ const setupBrowserTracker = () => {
     createTrackerConfig('_sp5_')
   )
   newTracker('biz1', 'https://c.snowplow.io', createTrackerConfig('_sp_biz1_'))
+
+  const selectedTabContext = () => {
+    const data = pickBy({
+      cloud: localStorage.getItem("docusaurus.tab.cloud"),
+      data_warehouse: localStorage.getItem("docusaurus.tab.warehouse")
+    })
+    if (!_.isEmpty(data)) return {
+      schema: 'iglu:com.snowplowanalytics.docs/selected_tabs/jsonschema/1-0-0',
+      data
+    }
+  }
+  addGlobalContexts([selectedTabContext])
 
   enableLinkClickTracking()
   refreshLinkClickTracking()

c64bd92e5bd1a6826d72d7729d971df53609f02d
gleb-lobov <126697986+gleb-lobov@users.noreply.github.com>
2023-08-11 13:53:53 +0200

    Update src/theme/MDXContent/index.js
    Co-authored-by: Nick <nick.stanch@gmail.com>

--- src/theme/MDXContent/index.js
+++ src/theme/MDXContent/index.js
@@ -24,12 +24,7 @@ export default function MDXContentWrapper(props) {
 
   const legacy = _.some(_.initial(breadcrumbs), item => item.customProps?.legacy)
   const outdated = !legacy && _.some(_.initial(breadcrumbs), item => item.customProps?.outdated)
-  let offerings; 
-  if (breadcrumbs) {
-    const currentPage = breadcrumbs.slice(-1)
-    const parents = breadcrumbs.slice(0, -1)
-    offerings = _.find(currentPage.map(x => Array.isArray(x.customProps?.offerings)).includes(true) ? currentPage : parents, item => item.customProps?.offerings)
-  }
+ const offerings = _.findLast(breadcrumbs, item => item.customProps?.offerings)
 
   if (outdated) {
     const latest = _.last(_.takeWhile(breadcrumbs, item => !item.customProps?.outdated)).href

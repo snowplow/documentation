8d7a0a76810468bff6ba53f23fe9b7b449d42b93
Nick <nick.stanch@snowplowanalytics.com>
2023-09-06 09:57:54 +0100

    Use GET method in FirstSteps to allow requests to HTTP

--- src/components/FirstSteps/index.tsx
+++ src/components/FirstSteps/index.tsx
@@ -37,7 +37,8 @@ const SnowplowSandbox = (props) => (
         ;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[]; p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments) };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1; n.src=w;g.parentNode.insertBefore(n,g)}}
         (window,document,"script","/js/sandboxed-sp.js","snowplow"));
         window.snowplow('newTracker', 'sp1', '${props.collectorUrl}', {
-          appId: '${props.appId}'
+          appId: '${props.appId}',
+          eventMethod: 'get'
         })
         window.run = function() {
           ${props.code}

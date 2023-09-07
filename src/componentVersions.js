8e6ac82bc1e404143a905922694735aaf549d55a
Benjamin BENOIST <benjamin.benoist@snowplowanalytics.com>
2023-09-07 15:26:13 +0000

    Enrich 3.8.2 and collector 2.9.2 (#603)

--- src/componentVersions.js
+++ src/componentVersions.js
@@ -22,8 +22,8 @@ export const versions = {
   webViewTracker: '0.2.0',
 
   // Core pipeline
-  collector: '2.9.1',
-  enrich: '3.8.1',
+  collector: '2.9.2',
+  enrich: '3.8.2',
   sqs2kinesis: '1.0.4',
   dataflowRunner: '0.7.3',
   snowbridge: '2.1.0',

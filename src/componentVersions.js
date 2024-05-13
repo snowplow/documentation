679819820ef29a8f5e655682ff5d3bf0d41163f4
Ryan Hill <rlh1994@users.noreply.github.com>
2024-05-13 09:12:42 +0100

    unified 0.4.1 (#880)
    * unified 0.4.1
    
    * Add feedback on passthrough information
    
    * reword thing
    
    * fix bad links

--- src/componentVersions.js
+++ src/componentVersions.js
@@ -42,7 +42,7 @@ export const versions = {
   // Data Modelling
   // dbt
   dbtSnowplowAttribution: '0.2.0',
-  dbtSnowplowUnified: '0.4.0',
+  dbtSnowplowUnified: '0.4.1',
   dbtSnowplowWeb: '1.0.1',
   dbtSnowplowMobile: '1.0.0',
   dbtSnowplowUtils: '0.16.5',

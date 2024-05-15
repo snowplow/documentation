6807aee92dca95764ea836a1c91e94ebe0acd841
Ryan Hill <rlh1994@users.noreply.github.com>
2024-05-15 16:43:58 +0100

    Add action to prep a dbt release given package and version (#886)
    * Add action to prep a dbt release given package and version
    
    * full media name

---
+++ .github/scripts/copy_schema.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+package_name="${1}"
+version="${2}"
+
+file_to_copy=$(find src/components/JsonSchemaValidator/Schemas -type f -name "dbt${package_name}*" | sort -V | tail -n 1)
+new_file_name=$(echo "${file_to_copy}" | sed "s/\(dbt${package_name}\).*\(\..*\)$/\1${version}\2/")
+cp "${file_to_copy}" "${new_file_name}"

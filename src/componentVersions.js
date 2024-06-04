3257b6dad4f6ff0de7f2cd2a1797914a9a2f1c46
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2024-06-04 15:37:05 +0300

    Upgrade enrich version to 4.2.1

--- src/componentVersions.js
+++ src/componentVersions.js
@@ -23,7 +23,7 @@ export const versions = {
 
   // Core pipeline
   collector: '3.1.0',
-  enrich: '4.1.0',
+  enrich: '4.2.1',
   sqs2kinesis: '1.0.4',
   dataflowRunner: '0.7.5',
   snowbridge: '2.4.1',

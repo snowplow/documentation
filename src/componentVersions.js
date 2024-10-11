39f3af1f98dce79e978ba6867481d11a8a0869cc
ilias1111 <4363375+ilias1111@users.noreply.github.com>
2024-10-11 12:07:54 +0000

    [create-pull-request] automated change

--- src/componentVersions.js
+++ src/componentVersions.js
@@ -49,7 +49,7 @@ export const versions = {
   dbtSnowplowMediaPlayer: '0.8.0',
   dbtSnowplowNormalize: '0.3.5',
   dbtSnowplowFractribution: '0.3.6',
-  dbtSnowplowEcommerce: '0.8.2',
+  dbtSnowplowEcommerce: '0.9.0',
 
   // sql runner
   sqlRunner: '0.10.1',

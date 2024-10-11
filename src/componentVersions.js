8ef8a806d03d52e85722910df9fc63f2d83134d8
ilias1111 <4363375+ilias1111@users.noreply.github.com>
2024-10-11 12:07:43 +0000

    [create-pull-request] automated change

--- src/componentVersions.js
+++ src/componentVersions.js
@@ -42,7 +42,7 @@ export const versions = {
   // Data Modelling
   // dbt
   dbtSnowplowAttribution: '0.3.0',
-  dbtSnowplowUnified: '0.4.5',
+  dbtSnowplowUnified: '0.5.0',
   dbtSnowplowWeb: '1.0.1',
   dbtSnowplowMobile: '1.0.0',
   dbtSnowplowUtils: '0.16.8',

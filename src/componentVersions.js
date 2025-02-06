8c2bb7e554bcf757802207f96998368158733575
Benjamin BENOIST <benjamin.benoist@snowplowanalytics.com>
2025-02-06 15:11:40 +0000

    Bump Elasticsearch loader to 2.1.3 (#1138)

--- src/componentVersions.js
+++ src/componentVersions.js
@@ -30,7 +30,7 @@ export const versions = {
 
   // Loaders
   bqLoader: '1.7.2',
-  esLoader: '2.1.2',
+  esLoader: '2.1.3',
   gcsLoader: '0.5.6',
   postgresLoader: '0.3.3',
   rdbLoader: '6.1.3',

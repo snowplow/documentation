27936c8df4fabfd0a10a806bb8c0113565dbd537
Miranda Wilson <miranda@snowplowanalytics.com>
2025-03-27 16:10:19 +0000

    Use cookie values for Product Fruits

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { getInitColorSchemeScript } from '@mui/material/styles'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from '@site/src/components/MuiTheme'
 import { ProductFruits } from 'react-product-fruits'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 
+const useCookie = () => {
+  const [userIdValue, setUserIdValue] = useState('')
+  const [sessionIdValue, setSessionIdValue] = useState('')
+
+  useEffect(() => {
+    const cookie = document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('_sp_biz1_id'))
+
+    const cookieExists = cookie !== undefined && cookie !== null
+
+    setUserIdValue(cookieExists ? cookie.split('=')[1].split('.')[0] : '')
+    setSessionIdValue(cookieExists ? cookie.split('=')[1].split('.')[5] : '')
+  })
+
+  return [
+    userIdValue ? userIdValue : 'unknown_user',
+    sessionIdValue ? sessionIdValue : 'unknown_session',
+  ]
+}
+
 export default function Root({ children }) {
   const { siteConfig } = useDocusaurusContext()
 
+  const [userId, sessionId] = useCookie()
+  console.log(userId, sessionId)
+
   const userInfo = {
-    username: 'test_user',
+    username: userId,
+    props: { sessionId: sessionId },
   }
   return (
     <>
@@ -17,6 +42,7 @@ export default function Root({ children }) {
         workspaceCode={siteConfig.customFields.productFruitsNext}
         language="en"
         user={userInfo}
+        lifeCycle="unmount"
       />
 
       {getInitColorSchemeScript()}

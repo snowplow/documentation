a19364b8d82c6ef631215ab9eba7844f2c00b45f
colmsnowplow <colm@snowplowanalytics.com>
2025-04-09 10:03:38 +0100

    Bump snowbrige componentVersion to 3.1.1 (#1144)
    * Bump snowbrige componentVersion to 3.1.1
    
    * Add new metrics

--- src/componentVersions.js
+++ src/componentVersions.js
@@ -25,7 +25,7 @@ export const versions = {
   enrich: '5.2.0',
   sqs2kinesis: '1.0.4',
   dataflowRunner: '0.7.5',
-  snowbridge: '3.0.1',
+  snowbridge: '3.1.1',
 
   // Loaders
   bqLoader: '2.0.0',

16b5caa9db5910223ed940c63c5f2e6b10299367
Miranda Wilson <miranda@snowplowanalytics.com>
2025-05-15 14:47:59 +0100

    Add MDX component exports

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from 'react'
 // Import the original mapper
-import MDXComponents from '@theme-original/MDXComponents';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import the FontAwesomeIcon component.
-import { library } from '@fortawesome/fontawesome-svg-core'; // Import the library component.
-import { fab } from '@fortawesome/free-brands-svg-icons'; // Import all brands icons.
-import { fas } from '@fortawesome/free-solid-svg-icons'; // Import all solid icons.
-import { far } from '@fortawesome/free-regular-svg-icons'; // Import all regular icons.
+import MDXComponents from '@theme-original/MDXComponents'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' // Import the FontAwesomeIcon component.
+import { library } from '@fortawesome/fontawesome-svg-core' // Import the library component.
+import { fab } from '@fortawesome/free-brands-svg-icons' // Import all brands icons.
+import { fas } from '@fortawesome/free-solid-svg-icons' // Import all solid icons.
+import { far } from '@fortawesome/free-regular-svg-icons' // Import all regular icons.
 
-library.add(fab, fas, far); // Add all icons to the library so you can use them without importing them individually.
+import ThemedImage from '@theme/ThemedImage'
+import ReactMarkdown from 'react-markdown'
+
+library.add(fab, fas, far) // Add all icons to the library so you can use them without importing them individually.
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
   Icon: FontAwesomeIcon, // Make the FontAwesomeIcon component available in MDX as <icon />.
-};
+  ThemedImage: ThemedImage, // Make ThemedImage available in MDX
+  ReactMarkdown: ReactMarkdown, // Make ReactMarkdown available in MDX
+}

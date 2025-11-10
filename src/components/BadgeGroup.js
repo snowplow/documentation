300985c048068dfaeb75011fe8bc78c552ec6b06
Alejandro Haydar <alejandro.haydar@snowplowanalytics.com>
2025-11-10 08:39:41 -0500

    Lowering Margin to on BadgeGroup Component
    From mt-10 to mt-5

--- src/components/BadgeGroup.js
+++ src/components/BadgeGroup.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function BadgeGroup({ children }) {
   return (
-    <div className="flex flex-wrap items-center gap-1 mt-10 mb-2">
+    <div className="flex flex-wrap items-center gap-1 mt-5 mb-2">
       {children}
     </div>
   )

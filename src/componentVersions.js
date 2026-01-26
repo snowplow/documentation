29e0bb26b92843adff60ee8d86d6763eedcdf173
Ian Streeter <ian@snowplowanalytics.com>
2026-01-26 14:58:39 +0000

    s3-loader 3.1.0 (#1603)

--- src/componentVersions.js
+++ src/componentVersions.js
@@ -34,7 +34,7 @@ export const versions = {
   gcsLoader: '0.5.6',
   postgresLoader: '0.3.3',
   rdbLoader: '6.2.0',
-  s3Loader: '3.0.0',
+  s3Loader: '3.1.0',
   s3Loader22x: '2.2.9',
   lakeLoader: '0.9.1',
   snowflakeStreamingLoader: '0.5.1',

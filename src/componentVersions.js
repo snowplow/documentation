cfc91421fdbfb74a72a7940128f457828d12feda
Piotr Poniedziałek <pondzix@gmail.com>
2026-05-20 12:55:13 +0200

    Bump snowbridge version to 5.1.1

--- src/componentVersions.js
+++ src/componentVersions.js
@@ -25,7 +25,7 @@ export const versions = {
   enrich: '6.11.0',
   sqs2kinesis: '1.0.4',
   dataflowRunner: '0.7.6',
-  snowbridge: '5.1.0',
+  snowbridge: '5.1.1',
 
   // Loaders
   bqLoader: '2.2.0',

32156d5d83e2d31ad6819b75c5e02a18e91883e2
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2026-06-22 18:43:17 +0300

    Bump GCS loader to 0.5.7 (#1840)

--- src/componentVersions.js
+++ src/componentVersions.js
@@ -31,7 +31,7 @@ export const versions = {
   bqLoader: '2.2.0',
   bqLoader1x: '1.7.2',
   esLoader: '3.0.1',
-  gcsLoader: '0.5.6',
+  gcsLoader: '0.5.7',
   postgresLoader: '0.3.3',
   rdbLoader: '6.5.0',
   s3Loader: '3.2.0',

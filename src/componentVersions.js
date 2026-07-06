4a43eeaf2a6fe85508a9560664533f7d8ef094d7
Piotr Poniedziałek <pondzix@gmail.com>
2026-07-06 15:32:02 +0200

    Snowbridge 5.2.0 (#1830)
    * Snowbridge 5.2.0
    
    * Mention the new context extraction functions in event forwarding
    
    * Update docs/destinations/forwarding-events/reference/index.md
    
    Co-authored-by: Nick <nick.stanch@snowplowanalytics.com>
    
    * Update docs/destinations/forwarding-events/reference/index.md
    
    Co-authored-by: Nick <nick.stanch@snowplowanalytics.com>
    
    * Update docs/destinations/forwarding-events/reference/index.md
    
    Co-authored-by: Nick <nick.stanch@snowplowanalytics.com>
    
    * Add Snowbridge to compression-supporting apps
    
    ---------
    
    Co-authored-by: Nick <nick.stanch@snowplowanalytics.com>

--- src/componentVersions.js
+++ src/componentVersions.js
@@ -25,7 +25,7 @@ export const versions = {
   enrich: '6.11.0',
   sqs2kinesis: '1.0.4',
   dataflowRunner: '0.7.8',
-  snowbridge: '5.1.1',
+  snowbridge: '5.2.0',
 
   // Loaders
   bqLoader: '2.2.1',
